3919bbfc93789dbd9e61bbc5e5b2614e52684b0d
Jane Zhou <pri17mib@gmail.com>
2019-04-02 23:50:16 +0100

    New folder for sd

---
+++ Crime_Street_Data_Cleaning.R
@@ -0,0 +1,42 @@
+crime_streets <- read_csv('south-yorkshire-street.csv')
+
+
+#Rename Column Month to Date 
+colnames(crime_streets)[2] <- "Date"
+
+#Check null values of variables for dimension
+sum(is.na(crime_streets$`Crime ID`))
+sum(is.na(crime_streets$Month))
+sum(is.na(crime_streets$`Reported by`))
+sum(is.na(crime_streets$`Falls within`))
+sum(is.na(crime_streets$Longitude))
+sum(is.na(crime_streets$Latitude))
+sum(is.na(crime_streets$Location))
+sum(is.na(crime_streets$`LSOA code`))
+sum(is.na(crime_streets$`LSOA name`))
+sum(is.na(crime_streets$`Crime type`))
+sum(is.na(crime_streets$`Last outcome category`))
+sum(is.na(crime_streets$Context))
+
+#Check the consistency of data types 
+table(sapply(crime_outcomes$Longitude, class))
+table(sapply(crime_outcomes$Latitude, class))
+table(sapply(crime_outcomes$`Crime ID`, class))
+
+#Check the month pattern
+bpa(crime_outcomes$Month, unique_only = TRUE)
+
+#Check the LSOA Code pattern
+bpa(crime_outcomes$`LSOA code`, unique_only = TRUE)  
+
+#Split month and year column
+crime_outcomes <- separate(data = crime_outcomes, col = Date, sep="[-]", remove=FALSE, convert=TRUE, into=crime_date)
+
+#Select & display invalid specified dates
+subset(crime_outcomes, Month <= 0 & Month > 12 || Year < 2015 & Year > 2018) 
+
+#Remove duplicates observations
+crime_outcomes <- unique(crime_outcomes)
+
+#Integrity Checks Between Datasets
+anti_join(crime_outcomes, crime_streets, by='Crime ID')
