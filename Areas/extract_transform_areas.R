bb03bc0f5cf3f69d8f6930095e3b1d0938d8fd78
Anna2802 <49158089+Anna2802@users.noreply.github.com>
2019-04-07 19:12:09 +0100

    Update extract_transform_areas.R
    Extracting, Cleansing and Transforming data:
    
    07/04/2019 - new comments
    
    07/04/2019 - extract_transform_areas.R split Month columns into year and month integer columns

--- Areas/extract_transform_areas.R
+++ Areas/extract_transform_areas.R
@@ -19,8 +19,9 @@ crimestreets <- read_csv("south-yorkshire-street.csv")
 #----
 # extract sheffield lsoa codes only
 lsoa_sheffield <- lsoa[ lsoa$LAD17NM == "Sheffield",]
+
 # Save in csv file
-#write.csv(lsoa_sheffield, file="lsoa-lookup-Sheffield-2011.csv")
+write.csv(lsoa_sheffield, file="lsoa-lookup-Sheffield-2011.csv")
 
 # Reduced table for LSOA codes: street LSOA code, street LSOA name, area LSOA code, area LSOA name
 lsoa_sheffield_red <- lsoa_sheffield[ ,c(1:4)]
@@ -51,7 +52,8 @@ crimestreets_nona <- crimestreets_nona[complete.cases(crimestreets_nona[,10]), ]
 # Reduced tables: Date, Street, Street LSOA code, street LSOA name, Crime type
 crimestreets_nona_red <- crimestreets_nona[ ,c(2,7:10)]
 
-t10 <- separate(data = crimestreets_nona_red, col = Month, into=c("Year","Month"), sep="[-]", remove=TRUE, convert=TRUE)
+# Split year and month into integer columns
+crimestreets_nona_red <- separate(data = crimestreets_nona_red, col = Month, into=c("Year","Month"), sep="[-]", remove=TRUE, convert=TRUE)
 
 #----
 # Merge crime streets with areas
@@ -63,6 +65,7 @@ t2<-data.frame(c1=character(),
                c4=character(),
                c5=character(),
                c6=character(),
+               c7=character(),
                stringsAsFactors=FALSE)
 
 # Column names
@@ -70,15 +73,22 @@ names(t2)<-names(crimestreets_area_red)
 
 # Add area column
 for(n in 1:nrow(lsoa_sheffield_nona)){
+  
+  # Select all rows with LSOA codes with index n
   t  <- crimestreets_area_red[crimestreets_area_red$`LSOA code`== as.character(lsoa_sheffield_nona[n,1]), ]
-  t1 <- t[complete.cases(t[ ,1]), ]
-  t1[ ,6] <- as.character(lsoa_sheffield_nona[n,4])
+  
+  # Remove all missing values in dates
+  t1 <- t[complete.cases(t[ ,c(1,2)]), ]
+  
+  # Add area name to empty column 7
+  t1[ ,7] <- as.character(lsoa_sheffield_nona[n,4])
+  
   # Add table t1 at the end of t2
   t2 <-rbind(t2,t1)
 }
 
 #Save results in csv file
-#write.csv(t2, file="crime-areas-Sheffield.csv")
+write.csv(t2, file="crime-areas-Sheffield.csv")
 
 
 
