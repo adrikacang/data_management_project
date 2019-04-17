0de1d8046c90c0c463b0efeb48c916caed43ad18
Adrian Septiandry <adrianseptiandry@gmail.com>
2019-04-17 18:02:04 +0100

    Update crime_data_transformation.R

--- crime_data_transformation.R
+++ crime_data_transformation.R
@@ -16,8 +16,8 @@ DimCrime <- select(crime_merge,c("Crime ID","Crime type","Last outcome category"
 colnames(DimCrime)<- c("Crime ID","Crime type","Last outcome category","Outcome type", "Street")
 
 #Time Dimension
-DimTime <- select(crime_merge, c("Month.x"))
-DimTime <- separate(data = DimTime, col = Month.x, sep="[-]", remove=FALSE, convert=TRUE, into=c("Year","Month"))
+DimTime <- select(crime_merge, c("Date.x"))
+DimTime <- separate(data = DimTime, col = Date.x, sep="[-]", remove=FALSE, convert=TRUE, into=c("Year","Month"))
 colnames(DimTime) <- c("Time ID", "Year", "Month")
 DimTime <- unique(DimTime)
 #Select only Outcome from DimCrime dataframe
