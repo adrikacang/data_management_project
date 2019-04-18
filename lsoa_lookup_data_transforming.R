ee929b49fb9d07b946e9884869964441cea5764f
adrikacang <adrianseptiandry@gmail.com>
2019-04-18 16:58:46 +0100

    Remove duplicate location in location dimension

--- lsoa_lookup_data_transforming.R
+++ lsoa_lookup_data_transforming.R
@@ -10,6 +10,7 @@ lsoa_lookup_merge <- merge(x=lsoa_lookup, y=crime_merge_lsoa, by='LSOA11CD')
 DimLocation<- lsoa_lookup_merge[ lsoa_lookup_merge$LAD17NM == "Sheffield" |  lsoa_lookup_merge$LAD17N == "Rotherham" | lsoa_lookup_merge$LAD17N == "Doncaster" |  lsoa_lookup_merge$LAD17N == "Barnsley", ]
 DimLocation <- select(DimLocation, c("LSOA11CD","WD17NM", "LAD17NM", "Location.x"))
 colnames(DimLocation) <- c("LSOA Code", "Area", "City", "Street")
+DimLocation <- unique(DimLocation)
 
 #Remove duplicates observations
 write.csv(lsoa_lookup_merge, file="Location Dimension.csv", row.names = FALSE)
