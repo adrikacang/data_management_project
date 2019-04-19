8d53813e0502c578fb1fdd6dc2c96debc16b627e
adrikacang <adrianseptiandry@gmail.com>
2019-04-19 22:40:34 +0100

    Add sheffield area only

--- Social deprivation/social_deprivation_dimension_and_fact.R
+++ Social deprivation/social_deprivation_dimension_and_fact.R
@@ -8,7 +8,6 @@ DimDeprivationJoin <- select(DimDeprivation, c("LSOA code (2011)", "Dim Deprivat
 ## Create fact table by merging crime dataset with deprivation dataset
 DimCrimeMerge <- DimCrime[DimCrime$`Outcome type` == '' | is.na(DimCrime$`Outcome type`), ]
 crime_streets_merge_dim <- merge(x = crime_streets, y = DimCrimeMerge, by.x ="Crime type", by.y = "Crime type")
-crime_q4_fact <- merge(x = crime_streets_merge_dim, y = DimDeprivationJoin, by.x="LSOA code", by.y = "LSOA code (2011)", all.x = TRUE)
+crime_dep_fact <- merge(x = crime_streets_merge_dim, y = DimDeprivationJoin, by.x="LSOA code", by.y = "LSOA code (2011)", all.x = TRUE)
+crime_dep_fact <- crime_dep_fact[complete.cases(crime_dep_fact[ ,c("Dim Deprivation ID")]),]
 
-#Select only specific column to be inserted into the table
-crime_dep_fact <- select(crime_q4_fact, c("Crime ID", "Date", "Dim Deprivation ID", "Dim Crime ID"))
