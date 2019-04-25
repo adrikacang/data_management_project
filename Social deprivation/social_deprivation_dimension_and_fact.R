001e0dfbb19aba9f609d638cda1c9ef722b199fa
adrikacang <adrianseptiandry@gmail.com>
2019-04-25 10:59:50 +0100

    Fix invalid column name

--- Social deprivation/social_deprivation_dimension_and_fact.R
+++ Social deprivation/social_deprivation_dimension_and_fact.R
@@ -16,7 +16,7 @@ crime_streets_merge_dim <- merge(x = crime_streets, y = DimCrimeMerge, by.x ="Cr
 crime_dep_fact <- merge(x = crime_streets_merge_dim, y = part1, by.x="LSOA code", by.y = "LSOA_code", all.x = TRUE)
 
 #Checking missing values of deprivation id and remove
-crime_dep_fact <- crime_dep_fact[complete.cases(crime_dep_fact[ ,c("Dim Deprivation ID")]),]
+crime_dep_fact <- crime_dep_fact[complete.cases(crime_dep_fact[ ,c("ID")]),]
 
 
 
